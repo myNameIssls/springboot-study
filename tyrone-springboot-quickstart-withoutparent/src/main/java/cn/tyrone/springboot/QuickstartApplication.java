29456e821fdc34d60d964fd497f41e830181bc97
ShangLishuai <lishuaishang@galaxyinternet.com>
2017-05-26 10:08:49 +0800

    the first commit

---
+++ tyrone-springboot-quickstart-withoutparent/src/main/java/cn/tyrone/springboot/QuickstartApplication.java
@@ -0,0 +1,23 @@
+package cn.tyrone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class QuickstartApplication {
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World!!!";
+    }
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(QuickstartApplication.class, args);
+	}
+
+}
