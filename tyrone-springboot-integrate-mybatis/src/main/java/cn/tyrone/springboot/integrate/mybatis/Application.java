e1ef1655d5b4142333e96c6f6d8f24c9d29e88b5
ShangLishuai <lishuaishang@galaxyinternet.com>
2017-05-26 14:49:21 +0800

    springboot integrate mybatis

---
+++ tyrone-springboot-integrate-mybatis/src/main/java/cn/tyrone/springboot/integrate/mybatis/Application.java
@@ -0,0 +1,31 @@
+package cn.tyrone.springboot.integrate.mybatis;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import cn.tyrone.springboot.integrate.mybatis.dao.UserMapper;
+
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan	
+public class Application implements CommandLineRunner {
+
+	@Autowired private UserMapper userMapper;
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+	public void run(String... arg0) throws Exception {
+		
+		List<Map<String, Object>> list = userMapper.list();
+        System.out.println(list);
+		
+		List<Map<String, Object>> listxml = userMapper.listxml();
+        System.out.println(listxml);
+	}
+
+}
---
+++ tyrone-springboot-integrate-mybatis/src/main/java/cn/tyrone/springboot/integrate/mybatis/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package cn.tyrone.springboot.integrate.mybatis.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+	
+	@Select(value = { "select * from sfb_capital_user_project" })
+	public List<Map<String, Object>> list();
+
+	public List<Map<String, Object>> listxml();
+}
