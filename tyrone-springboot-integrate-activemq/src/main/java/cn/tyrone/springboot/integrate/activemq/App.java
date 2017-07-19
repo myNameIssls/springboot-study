ec601537f127a3b262b30dbf93aaa0f74103d190
ShangLishuai <lishuaishang@galaxyinternet.com>
2017-07-19 17:13:06 +0800

    add redis message's demo and activeMQ's demo

---
+++ tyrone-springboot-integrate-activemq/src/main/java/cn/tyrone/springboot/integrate/activemq/App.java
@@ -0,0 +1,13 @@
+package cn.tyrone.springboot.integrate.activemq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tyrone-springboot-integrate-activemq/src/main/java/cn/tyrone/springboot/integrate/activemq/Producer.java
@@ -0,0 +1,21 @@
+package cn.tyrone.springboot.integrate.activemq;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消息生产者
+ * 
+ * @author Administrator
+ *
+ */
+//@com
+public class Producer implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
