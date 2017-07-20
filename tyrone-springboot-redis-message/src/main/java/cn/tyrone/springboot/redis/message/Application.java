7ea973752cc76570dbadee90e6dde2ed53946330
ShangLishuai <lishuaishang@galaxyinternet.com>
2017-07-20 10:29:18 +0800

    add notes

--- tyrone-springboot-redis-message/src/main/java/cn/tyrone/springboot/redis/message/Application.java
+++ tyrone-springboot-redis-message/src/main/java/cn/tyrone/springboot/redis/message/Application.java
@@ -20,21 +20,31 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 public class Application {
 	public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 	
+	/*
+	 * Redis消息监听器容器
+	 * 这个容器加载了RedisConnectionFactory和消息监听器
+	 */
 	@Bean
 	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory, 
 			MessageListenerAdapter listenerAdapter){
-		
 		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 		container.setConnectionFactory(connectionFactory);
 		container.addMessageListener(listenerAdapter, new PatternTopic("sprinboot-redis-messaage"));
 		return container;
 	}
 	
+	/*
+	 * 将Receiver注册为一个消息监听器，并指定消息接收的方法（receiveMessage）
+	 * 如果不指定消息接收的方法，消息监听器会默认的寻找Receiver中的handleMessage这个方法作为消息接收的方法
+	 */
 	@Bean
 	MessageListenerAdapter listenerAdapter(Receiver receiver){
 		return new MessageListenerAdapter(receiver, "receiveMessage");
 	}
 	
+	/*
+	 * Receiver实例
+	 */
 	@Bean
 	Receiver receiver(CountDownLatch latch){
 		return new Receiver(latch);
@@ -45,25 +55,29 @@ public class Application {
 		return new CountDownLatch(1);
 	}
 	
+	/*
+	 * Redis Template 用来发送消息
+	 */
 	@Bean
 	StringRedisTemplate template(RedisConnectionFactory connectionFactory){
 		return new StringRedisTemplate(connectionFactory);
 	}
 	
+	/*
+	 * 测试用例
+	 */
 	public static void main(String[] args) throws Exception {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 		
 		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
-//		CountDownLatch latch = ctx.getBean(CountDownLatch.class);
+		CountDownLatch latch = ctx.getBean(CountDownLatch.class);
 		
 		LOGGER.info("Sending message......");
 		template.convertAndSend("sprinboot-redis-messaage", "Hello, SpringBoot redis message!!!!");
-//		latch.wait();
+		latch.wait();
 		
 		System.exit(0);
 		
 	}
-
-	
 	
 }
