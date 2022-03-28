6b3678b54d55a6c83e622b9ed1e9e11821265652
尚立帅 <shanglishuai@iciyun.com>
2022-03-28 09:54:24 +0800

    升级版本

--- tyrone-springboot-integrate-thymeleaf3/src/main/java/cn/tyrone/springboot/thymeleaf3/controller/MessageController.java
+++ tyrone-springboot-integrate-thymeleaf3/src/main/java/cn/tyrone/springboot/thymeleaf3/controller/MessageController.java
@@ -1,19 +1,14 @@
 package cn.tyrone.springboot.thymeleaf3.controller;
 
-import javax.validation.Valid;
-
+import cn.tyrone.springboot.thymeleaf3.Message;
+import cn.tyrone.springboot.thymeleaf3.MessageRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import cn.tyrone.springboot.thymeleaf3.Message;
-import cn.tyrone.springboot.thymeleaf3.MessageRepository;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
